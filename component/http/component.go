01c24a92adf5c4545d024c595fc73bd3fe08cfc6
Rein Krul <info@reinkrul.nl>
2025-08-06 07:38:25 +0200

    #46: component structure

---
+++ component/http/component.go
@@ -0,0 +1,50 @@
+package http
+
+import (
+	"context"
+	"errors"
+	"github.com/nuts-foundation/nuts-knooppunt/component"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+var _ component.Lifecycle = (*Component)(nil)
+
+type Component struct {
+	mux    *http.ServeMux
+	server *http.Server
+}
+
+func New(mux *http.ServeMux) *Component {
+	return &Component{
+		mux: mux,
+	}
+}
+
+func (c *Component) Start() error {
+	const addr = ":8080"
+	c.server = &http.Server{
+		Addr:    addr,
+		Handler: c.mux,
+	}
+	log.Info().Msgf("Public interface listens on %s", addr)
+	go func() {
+		if err := c.server.ListenAndServe(); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				log.Err(err).Msg("Failed to start server")
+			}
+		}
+	}()
+	return nil
+}
+
+func (c *Component) Stop(ctx context.Context) error {
+	return c.server.Shutdown(ctx)
+}
+
+func (c *Component) RegisterHttpHandlers(mux *http.ServeMux) {
+	// Nothing to do here
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello, World!"))
+	})
+}
---
+++ component/status/component.go
@@ -0,0 +1,33 @@
+package status
+
+import (
+	"context"
+	"github.com/nuts-foundation/nuts-knooppunt/component"
+	"net/http"
+)
+
+var _ component.Lifecycle = (*Component)(nil)
+
+type Component struct {
+}
+
+func New() *Component {
+	return &Component{}
+}
+
+func (c Component) Start() error {
+	// Nothing to do
+	return nil
+}
+
+func (c Component) Stop(ctx context.Context) error {
+	// Nothing to do
+	return nil
+}
+
+func (c Component) RegisterHttpHandlers(mux *http.ServeMux) {
+	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+}
