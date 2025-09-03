8fce6ed156fce22835830a928447ac7101354f2c
Joris Scharp <joris@headease.nl>
2025-09-03 14:29:30 +0200

    refactor(mcsd): extract test setup duplication

--- component/mcsd/component_test.go
+++ component/mcsd/component_test.go
@@ -15,17 +15,19 @@ import (
 	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
 )
 
-func TestComponent_update(t *testing.T) {
-	t.Log("mCSD Component is tested limited here, as it requires running FHIR servers and a lot of data. The main logic is tested in the integration tests.")
+type testSetup struct {
+	component    *Component
+	server       *httptest.Server
+	localClient  *test.StubFHIRClient
+	testDataJSON []byte
+}
+
+func setupTest(t *testing.T, historyHandler http.HandlerFunc) *testSetup {
 	testDataJSON, err := os.ReadFile("test/test_bundle_with_directories.json")
 	require.NoError(t, err)
 
 	rootDirMux := http.NewServeMux()
-	rootDirMux.HandleFunc("/_history", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/fhir+json")
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(testDataJSON)
-	})
+	rootDirMux.HandleFunc("/_history", historyHandler)
 	rootDirServer := httptest.NewServer(rootDirMux)
 
 	localClient := &test.StubFHIRClient{}
@@ -48,76 +50,69 @@ func TestComponent_update(t *testing.T) {
 		}
 		panic("unknown base URL: " + baseURL.String())
 	}
+
+	return &testSetup{
+		component:    component,
+		server:       rootDirServer,
+		localClient:  localClient,
+		testDataJSON: testDataJSON,
+	}
+}
+
+func TestComponent_update(t *testing.T) {
+	t.Log("mCSD Component is tested limited here, as it requires running FHIR servers and a lot of data. The main logic is tested in the integration tests.")
+	
+	var setup *testSetup
+	setup = setupTest(t, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/fhir+json")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(setup.testDataJSON)
+	})
 	ctx := context.Background()
 
-	report, err := component.update(ctx)
+	report, err := setup.component.update(ctx)
 
 	require.NoError(t, err)
 	require.NotNil(t, report)
-	require.NoError(t, report[rootDirServer.URL].Error)
-	require.Empty(t, report[rootDirServer.URL].Warnings)
+	require.NoError(t, report[setup.server.URL].Error)
+	require.Empty(t, report[setup.server.URL].Warnings)
 
 	t.Run("check created resources", func(t *testing.T) {
-		require.Len(t, localClient.CreatedResources["Bundle"], 1)
-		bundle := localClient.CreatedResources["Bundle"][0].(fhir.Bundle)
+		require.Len(t, setup.localClient.CreatedResources["Bundle"], 1)
+		bundle := setup.localClient.CreatedResources["Bundle"][0].(fhir.Bundle)
 		require.Len(t, bundle.Entry, 9)
 	})
 }
 
 func TestComponent_incrementalUpdates(t *testing.T) {
-	testDataJSON, err := os.ReadFile("test/test_bundle_with_directories.json")
-	require.NoError(t, err)
-
 	var sinceParams []string
-	rootDirMux := http.NewServeMux()
-	rootDirMux.HandleFunc("/_history", func(w http.ResponseWriter, r *http.Request) {
+	var setup *testSetup
+	setup = setupTest(t, func(w http.ResponseWriter, r *http.Request) {
 		// FHIR client uses POST by default, parameters are in form data
 		r.ParseForm()
 		since := r.Form.Get("_since")
 		sinceParams = append(sinceParams, since)
 		w.Header().Set("Content-Type", "application/fhir+json")
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(testDataJSON)
+		_, _ = w.Write(setup.testDataJSON)
 	})
-	rootDirServer := httptest.NewServer(rootDirMux)
-
-	config := Config{
-		RootDirectories: map[string]DirectoryConfig{
-			"rootDir": {
-				FHIRBaseURL: rootDirServer.URL,
-			},
-		},
-		LocalDirectory: DirectoryConfig{
-			FHIRBaseURL: "http://example.com/local/fhir",
-		},
-	}
-	component := New(config)
-	component.fhirClientFn = func(baseURL *url.URL) fhirclient.Client {
-		if baseURL.String() == rootDirServer.URL {
-			return fhirclient.New(baseURL, http.DefaultClient, nil)
-		}
-		if baseURL.String() == "http://example.com/local/fhir" {
-			return &test.StubFHIRClient{}
-		}
-		panic("unknown base URL: " + baseURL.String())
-	}
 	ctx := context.Background()
 
 	// First update - should have no _since parameter
-	_, err = component.update(ctx)
+	_, err := setup.component.update(ctx)
 	require.NoError(t, err)
 	require.Len(t, sinceParams, 1, "Should have one request")
 	require.Empty(t, sinceParams[0], "First update should not have _since parameter")
 	
 	// Verify timestamp was stored
-	component.lastUpdateTimesMux.RLock()
-	lastUpdate, exists := component.lastUpdateTimes[rootDirServer.URL]
-	component.lastUpdateTimesMux.RUnlock()
+	setup.component.lastUpdateTimesMux.RLock()
+	lastUpdate, exists := setup.component.lastUpdateTimes[setup.server.URL]
+	setup.component.lastUpdateTimesMux.RUnlock()
 	require.True(t, exists, "Last update time should be stored")
 	require.WithinDuration(t, time.Now(), lastUpdate, 5*time.Second)
 
 	// Second update - should include _since parameter
-	_, err = component.update(ctx)
+	_, err = setup.component.update(ctx)
 	require.NoError(t, err)
 	require.Len(t, sinceParams, 2, "Should have two requests total")
 	require.NotEmpty(t, sinceParams[1], "Second update should include _since parameter")
