9a37aa4e53bfee1e73bc4bdd6000f841738e0bd1
Dirk Geurs <dirk.geurs@reaktor.com>
2025-09-11 12:20:17 +0200

    Fix colon

--- component/mcsdadmin/static/js/lib.js
+++ component/mcsdadmin/static/js/lib.js
@@ -1,6 +1,6 @@
 function addOption(elementId) {
     var option = document.getElementById(elementId);
     var newOption = option.cloneNode(true);
-    newOption.id = null
+    newOption.id = null;
     option.parentElement.appendChild(newOption);
 }
