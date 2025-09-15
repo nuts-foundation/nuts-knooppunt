57ef65d66da3db493016b4ac11a2f64ba6b9e883
Rein Krul <info@reinkrul.nl>
2025-09-15 10:26:00 +0200

    fix

--- lib/from/json.go
+++ lib/from/json.go
@@ -12,7 +12,7 @@ func JSONResponse[T any](httpResponse *http.Response) (T, error) {
 	var result T
 	if httpResponse.StatusCode < 200 || httpResponse.StatusCode >= 300 {
 		responseData, _ := io.ReadAll(httpResponse.Body)
-		return result, fmt.Errorf("non-OK status code (status=%, url=%s)\nResponse data:\n----------------\n%s\n----------------", httpResponse.Status, httpResponse.Request.URL, strings.TrimSpace(string(responseData)))
+		return result, fmt.Errorf("non-OK status code (status=%s, url=%s)\nResponse data:\n----------------\n%s\n----------------", httpResponse.Status, httpResponse.Request.URL, strings.TrimSpace(string(responseData)))
 	}
 	if err := json.NewDecoder(httpResponse.Body).Decode(&result); err != nil {
 		return result, fmt.Errorf("failed to decode response body: %w", err)
