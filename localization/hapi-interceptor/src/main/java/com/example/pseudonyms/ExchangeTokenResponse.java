ed03350c6c2f4fb691f70c715fe0156a1e9b6272
stevenvegt <steven.vandervegt@nedap.com>
2025-09-18 11:49:14 +0200

    Adds pseudonym support for NVI to HAPI
    Adds a HAPI interceptor which perform the following operations for a
    docurmentReference:
    
    On storage exchange a nvi-pseudonym-token to a nvi pseudonym
    On retrieval exchange a nvi-pseudonym to the requestor-pseudonym-token
    
    FHIR Clients need to create the nvi-pseudo-token before publishing and
    exchange the token for a BSN after retrieval by directly interacting
    with the pseudonym service.

---
+++ localization/hapi-interceptor/src/main/java/com/example/pseudonyms/ExchangeTokenResponse.java
@@ -0,0 +1,7 @@
+package com.example.fhirserver.pseudonyms;
+
+public class ExchangeTokenResponse {
+    public Identifier identifier;
+
+    public ExchangeTokenResponse() {}
+}
---
+++ localization/hapi-interceptor/src/main/java/com/example/pseudonyms/GetTokenResponse.java
@@ -0,0 +1,7 @@
+package com.example.fhirserver.pseudonyms;
+
+public class GetTokenResponse {
+    public String token;
+
+    public GetTokenResponse() {}
+}
