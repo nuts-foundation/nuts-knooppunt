5590097b79f5986f57c1f3af66ca08b4fb78f854
Casper Schuijt <casper@cschuijt.nl>
2025-10-07 13:35:44 +0200

    start testing organization validator

---
+++ component/mcsd/validator_test.go
@@ -0,0 +1,58 @@
+package mcsd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zorgbijjou/golang-fhir-models/fhir-models/caramel/to"
+	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
+)
+
+func TestOrganizationValidator(t *testing.T) {
+	organizations := []struct {
+		name         string
+		organization fhir.Organization
+		valid        bool
+	}{
+		{
+			name: "valid Organization",
+			organization: fhir.Organization{
+				Identifier: []fhir.Identifier{
+					{
+						System: to.Ptr("http://fhir.nl/fhir/NamingSystem/ura"),
+						Value:  to.Ptr("12345"),
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			name: "Organization with double URA",
+			organization: fhir.Organization{
+				Identifier: []fhir.Identifier{
+					{
+						System: to.Ptr("http://fhir.nl/fhir/NamingSystem/ura"),
+						Value:  to.Ptr("12346"),
+					},
+					{
+						System: to.Ptr("http://fhir.nl/fhir/NamingSystem/ura"),
+						Value:  to.Ptr("12345"),
+					},
+				},
+			},
+			valid: false,
+		},
+	}
+
+	for _, tt := range organizations {
+		t.Run(tt.name, func(t *testing.T) {
+			err2 := validateOrganizationResource(t.Context(), &tt.organization)
+
+			if tt.valid {
+				require.NoError(t, err2)
+			} else {
+				require.Error(t, err2)
+			}
+		})
+	}
+}
