9f5cc137496982264eaae5ba0ed7946b1de972ef
Casper Schuijt <casper@cschuijt.nl>
2025-10-14 11:01:16 +0200

    add basic validations for non-Organization resources

--- component/mcsd/validator_test.go
+++ component/mcsd/validator_test.go
@@ -8,6 +8,44 @@ import (
 	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
 )
 
+// Test that the generic validators do their job. At this point, this only checks whether
+// the resource is of an allowed type before deferring to a resource-specific function.
+func TestGenericValidators(t *testing.T) {
+	resources := []struct {
+		name         string
+		resourceJSON []byte
+		valid        bool
+	}{
+		{
+			name:         "valid Organization",
+			resourceJSON: []byte(`{"resourceType":"Organization", "identifier":[{"system":"http://fhir.nl/fhir/NamingSystem/ura","value":"12345"}]}`),
+			valid:        true,
+		},
+		{
+			name:         "invalid resource type",
+			resourceJSON: []byte(`{"resourceType":"Patient"}`),
+			valid:        false,
+		},
+	}
+
+	for _, tt := range resources {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ValidateUpdate(t.Context(), ValidationRules{AllowedResourceTypes: []string{"Organization"}}, tt.resourceJSON)
+
+			if tt.valid {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+		})
+	}
+
+}
+
+// Test validator for Organization resources. An organization should either have an URA number
+// or should be a sub-organization to an Organization with an URA number.
+//
+// https://nuts-foundation.github.io/nl-generic-functions-ig/care-services.html#update-client
 func TestOrganizationValidator(t *testing.T) {
 	organizations := []struct {
 		name         string
@@ -79,12 +117,12 @@ func TestOrganizationValidator(t *testing.T) {
 
 	for _, tt := range organizations {
 		t.Run(tt.name, func(t *testing.T) {
-			err2 := validateOrganizationResource(t.Context(), &tt.organization)
+			err := validateOrganizationResource(t.Context(), &tt.organization)
 
 			if tt.valid {
-				require.NoError(t, err2)
+				require.NoError(t, err)
 			} else {
-				require.Error(t, err2)
+				require.Error(t, err)
 			}
 		})
 	}
