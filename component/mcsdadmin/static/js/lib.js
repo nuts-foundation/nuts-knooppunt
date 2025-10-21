5ad3387626d6a9f077b354d126d24ba12acf51f7
Dirk Geurs <dirk.geurs@reaktor.com>
2025-10-21 16:19:31 +0200

    wip

--- component/mcsdadmin/static/js/lib.js
+++ component/mcsdadmin/static/js/lib.js
@@ -1,21 +1,45 @@
 function addOption(elementId) {
-    var option = document.getElementById(elementId);
-    var newOption = option.cloneNode(true);
-    newOption.id = null;
-    newOption.required = false;
-    option.parentElement.appendChild(newOption);
+    let option = document.getElementById(elementId);
+    let newOption = option.cloneNode(true);
+    let complexOption = option.tagName === "DIV"
+    
+    if (complexOption) {
+        // Update the names of the child nodes in case of a complex option
+        let childNodes = Array.from(newOption.children)
+        childNodes.forEach(child => {
+            let name = child.name
+            child.name = incrementIndex(name)
+            
+        })
+    } else {
+        // Just copy the option in case of a simple option
+        newOption.id = null;
+        newOption.required = false;
+        
+        option.parentElement.appendChild(newOption);
+    }
 }
 
+const indexRe = /.+\[(\d+)\].+/;
+function incrementIndex (name) {
+    if (typeof name === "string") {
+        console.log(name)
+        console.log(name.match(re))
+    } else {
+        return name
+    }
+} 
+
 function removeOption(elementId) {
-    var optionDiv = document.getElementById(elementId);
-    var childrenCount = optionDiv.children.length;
+    let optionDiv = document.getElementById(elementId);
+    let childrenCount = optionDiv.children.length;
     if (childrenCount > 1) {
         optionDiv.removeChild(optionDiv.lastChild);
     }
 }
 
 function dismissAlert(elementId) {
-    var elm = document.getElementById(elementId);
+    let elm = document.getElementById(elementId);
     elm.hidden = true
 }
 
