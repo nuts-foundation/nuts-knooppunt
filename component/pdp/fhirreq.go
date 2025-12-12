3181a7f417b754e390378f74d452c429b8abaa89
Dirk Geurs <dirk.geurs@reaktor.com>
2025-12-12 17:05:02 +0100

    new policy input

--- component/pdp/fhirreq.go
+++ component/pdp/fhirreq.go
@@ -6,13 +6,13 @@ import (
 	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
 )
 
-type Definition struct {
+type PathDef struct {
 	Interaction fhir.TypeRestfulInteraction
 	PathDef     []string
 	Verb        string
 }
 
-var definitions = []Definition{
+var definitions = []PathDef{
 	{
 		Interaction: fhir.TypeRestfulInteractionRead,
 		PathDef:     []string{"[type]", "[id]"},
@@ -36,12 +36,13 @@ var definitions = []Definition{
 }
 
 type Tokens struct {
-	ResourceType  fhir.ResourceType
+	Interaction   fhir.TypeRestfulInteraction
+	ResourceType  *fhir.ResourceType
 	ResourceId    string
 	OperationName string
 }
 
-func parseDefinition(def Definition, req HTTPRequest) (Tokens, bool) {
+func parsePath(def PathDef, req HTTPRequest) (Tokens, bool) {
 	var out Tokens
 
 	if def.Verb != req.Method {
@@ -64,22 +65,19 @@ func parseDefinition(def Definition, req HTTPRequest) (Tokens, bool) {
 		isToken := strings.HasPrefix(part, "[")
 		switch part {
 		case "[type]":
-			var t fhir.ResourceType
-			err := t.UnmarshalJSON([]byte(path[idx]))
-			if err != nil {
+			ptr, ok := parseResourceType(path[idx])
+			if !ok {
 				return Tokens{}, false
 			}
-			out.ResourceType = t
+			out.ResourceType = ptr
 			continue
 		case "[type]?":
 			str := strings.TrimSuffix(path[idx], "?")
-
-			var t fhir.ResourceType
-			err := t.UnmarshalJSON([]byte(str))
-			if err != nil {
+			ptr, ok := parseResourceType(str)
+			if !ok {
 				return Tokens{}, false
 			}
-			out.ResourceType = t
+			out.ResourceType = ptr
 			continue
 		case "$[name]":
 			out.OperationName = strings.TrimPrefix(path[idx], "$")
@@ -99,6 +97,63 @@ func parseDefinition(def Definition, req HTTPRequest) (Tokens, bool) {
 	}
 
 	return out, true
+}
+
+func parseResourceType(str string) (*fhir.ResourceType, bool) {
+	var t fhir.ResourceType
+	err := t.UnmarshalJSON([]byte(str))
+	if err != nil {
+		return nil, false
+	}
+	return &t, true
+}
+
+func parseRequest(request HTTPRequest) (Tokens, bool) {
+	var tokens Tokens
+	var def PathDef
+	var ok bool
+	for _, d := range definitions {
+		tokens, ok = parsePath(def, request)
+		if ok {
+			def = d
+			break
+		}
+	}
+
+	if !ok {
+		return tokens, false
+	}
+
+	tokens.Interaction = def.Interaction
+	return tokens, true
+}
+
+func NewPolicyInput(request PDPRequest) (PolicyInput, bool) {
+	var policyInput PolicyInput
+
+	tokens, ok := parseRequest(request.Input.Request)
+	if !ok {
+		return PolicyInput{}, false
+	}
+
+	if tokens.ResourceType != nil {
+		policyInput.Resource.Type = *tokens.ResourceType
+		if tokens.ResourceId != "" {
+			policyInput.Resource.Properties.ResourceId = tokens.ResourceId
+		}
+	}
+
+	policyInput.Action.Properties = PolicyActionProperties{
+		InteractionType: tokens.Interaction,
+	}
+
+	if tokens.OperationName != "" {
+		policyInput.Action.Properties.Operation = &tokens.OperationName
+	}
+
+	// TODO: Place query params
+
+	return policyInput, true
 }
 
 // https://hl7.org/fhir/R4/http.html
--- component/pdp/fhirreq_test.go
+++ component/pdp/fhirreq_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestComponent_parse_tokens(t *testing.T) {
-	var def = Definition{
+	var def = PathDef{
 		Interaction: fhir.TypeRestfulInteractionRead,
 		PathDef:     []string{"[type]", "[id]"},
 		Verb:        "GET",
@@ -18,15 +18,15 @@ func TestComponent_parse_tokens(t *testing.T) {
 		Method: "GET",
 		Path:   "/Observation/12775",
 	}
-	tokens, ok := parseDefinition(def, req)
+	tokens, ok := parsePath(def, req)
 
 	assert.True(t, ok)
 	assert.Equal(t, "12775", tokens.ResourceId)
-	assert.Equal(t, fhir.ResourceTypeObservation, tokens.ResourceType)
+	assert.Equal(t, fhir.ResourceTypeObservation, *tokens.ResourceType)
 }
 
 func TestComponent_parse_literals(t *testing.T) {
-	var def = Definition{
+	var def = PathDef{
 		Interaction: fhir.TypeRestfulInteractionHistorySystem,
 		PathDef:     []string{"_history"},
 		Verb:        "GET",
@@ -36,13 +36,13 @@ func TestComponent_parse_literals(t *testing.T) {
 		Method: "GET",
 		Path:   "/_history",
 	}
-	_, ok := parseDefinition(def, req)
+	_, ok := parsePath(def, req)
 
 	assert.True(t, ok)
 }
 
 func TestComponent_parse_trailing_question_mark(t *testing.T) {
-	var def = Definition{
+	var def = PathDef{
 		Interaction: fhir.TypeRestfulInteractionSearchType,
 		PathDef:     []string{"[type]?"},
 		Verb:        "GET",
@@ -52,14 +52,14 @@ func TestComponent_parse_trailing_question_mark(t *testing.T) {
 		Method: "GET",
 		Path:   "/Observation?",
 	}
-	tokens, ok := parseDefinition(def, req)
+	tokens, ok := parsePath(def, req)
 
 	assert.True(t, ok)
-	assert.Equal(t, fhir.ResourceTypeObservation, tokens.ResourceType)
+	assert.Equal(t, fhir.ResourceTypeObservation, *tokens.ResourceType)
 }
 
 func TestComponent_parse_leading_dollar(t *testing.T) {
-	var def = Definition{
+	var def = PathDef{
 		Interaction: fhir.TypeRestfulInteractionOperation,
 		PathDef:     []string{"[type]", "[id]", "$[name]"},
 		Verb:        "GET",
@@ -69,7 +69,7 @@ func TestComponent_parse_leading_dollar(t *testing.T) {
 		Method: "GET",
 		Path:   "/Observation/123123/$validate",
 	}
-	tokens, ok := parseDefinition(def, req)
+	tokens, ok := parsePath(def, req)
 
 	assert.True(t, ok)
 	assert.Equal(t, "validate", tokens.OperationName)
