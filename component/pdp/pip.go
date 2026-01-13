eef6a779cc0084386b09dbb22e604623a3d203c4
Dirk Geurs <dirk.geurs@reaktor.com>
2026-01-13 13:16:12 +0100

    add example request

--- component/pdp/pip.go
+++ component/pdp/pip.go
@@ -23,8 +23,8 @@ func PipPolicyInput(c Component, policyInput PolicyInput) PolicyInput {
 
 		var patient fhir.Patient
 		path := fmt.Sprintf("Patient/%s", patientId)
-		err := client.Read(path, patient)
-		if err == nil {
+		err := client.Read(path, &patient)
+		if err != nil {
 			slog.Warn("Failed to get patient record from PIP, policy input might not be complete", logging.Error(err))
 			return policyInput
 		}
