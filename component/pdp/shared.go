6047d10f612ea51112b4bf3c97d26a92ced2d884
Dirk Geurs <dirk.geurs@reaktor.com>
2026-01-19 10:31:13 +0100

    Update component/pdp/shared.go
    Co-authored-by: reinkrul <info@reinkrul.nl>

--- component/pdp/shared.go
+++ component/pdp/shared.go
@@ -159,7 +159,7 @@ type PIPConfig struct {
 
 type Config struct {
 	Enabled bool      `koanf:"enabled"`
-	PIP     PIPConfig `koanf:"pipurl"`
+	PIP     PIPConfig `koanf:"pip"`
 }
 
 type Component struct {
