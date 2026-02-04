80e7198ca1b0d22088d748c0ee08d8e74992dbaa
Rein Krul <info@reinkrul.nl>
2026-02-04 19:54:22 +0100

    fix

--- component/pdp/capabilities_test.go
+++ component/pdp/capabilities_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/nuts-foundation/nuts-knooppunt/lib/to"
 	"github.com/stretchr/testify/assert"
 	"github.com/zorgbijjou/golang-fhir-models/fhir-models/fhir"
 )
@@ -33,217 +34,8 @@ func TestComponent_reject_interaction(t *testing.T) {
 		},
 	}
 
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.False(t, resp.Allow)
-	assert.False(t, inp.Context.FHIRCapabilityChecked)
-}
-
-func TestComponent_allow_interaction(t *testing.T) {
-	input := PolicyInput{
-		Subject: Subject{
-			Properties: SubjectProperties{
-				ClientQualifications:  []string{"mcsd_update"},
-				SubjectOrganizationId: "00000666",
-			},
-		},
-		Resource: PolicyResource{
-			Type: fhir.ResourceTypeOrganization,
-			Properties: PolicyResourceProperties{
-				ResourceId: "118876",
-			},
-		},
-		Action: PolicyAction{
-			Properties: PolicyActionProperties{
-				ContentType:     "application/fhir+json",
-				InteractionType: fhir.TypeRestfulInteractionHistoryType,
-			},
-		},
-		Context: PolicyContext{
-			DataHolderOrganizationId: "00000659",
-		},
-	}
-
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.True(t, resp.Allow)
-	assert.True(t, inp.Context.FHIRCapabilityChecked)
-}
-
-func TestComponent_allow_search_param(t *testing.T) {
-	input := PolicyInput{
-		Subject: Subject{
-			Properties: SubjectProperties{
-				ClientQualifications:  []string{"mcsd_update"},
-				SubjectOrganizationId: "00000666",
-			},
-		},
-		Resource: PolicyResource{
-			Type: fhir.ResourceTypeOrganization,
-			Properties: PolicyResourceProperties{
-				ResourceId: "118876",
-			},
-		},
-		Action: PolicyAction{
-			Properties: PolicyActionProperties{
-				ContentType:     "application/fhir+json",
-				InteractionType: fhir.TypeRestfulInteractionSearchType,
-				SearchParams:    map[string]string{"_since": "2024-01-01"},
-			},
-		},
-		Context: PolicyContext{
-			DataHolderOrganizationId: "00000659",
-		},
-	}
-
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.True(t, resp.Allow)
-	assert.True(t, inp.Context.FHIRCapabilityChecked)
-}
-
-func TestComponent_reject_search_param(t *testing.T) {
-	input := PolicyInput{
-		Subject: Subject{
-			Properties: SubjectProperties{
-				ClientQualifications:  []string{"mcsd_update"},
-				SubjectOrganizationId: "00000666",
-			},
-		},
-		Resource: PolicyResource{
-			Type: fhir.ResourceTypeOrganization,
-			Properties: PolicyResourceProperties{
-				ResourceId: "118876",
-			},
-		},
-		Action: PolicyAction{
-			Properties: PolicyActionProperties{
-				ContentType:     "application/fhir+json",
-				InteractionType: fhir.TypeRestfulInteractionSearchType,
-				SearchParams:    map[string]string{"_foo": "bar", "_since": "2024-01-01"},
-			},
-		},
-		Context: PolicyContext{
-			DataHolderOrganizationId: "00000659",
-		},
-	}
-
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.False(t, resp.Allow)
-	assert.False(t, inp.Context.FHIRCapabilityChecked)
-}
-
-func TestComponent_reject_interaction_type(t *testing.T) {
-	input := PolicyInput{
-		Subject: Subject{
-			Properties: SubjectProperties{
-				ClientQualifications:  []string{"mcsd_update"},
-				SubjectOrganizationId: "00000666",
-			},
-		},
-		Action: PolicyAction{
-			Properties: PolicyActionProperties{
-				ContentType:     "application/fhir+json",
-				InteractionType: fhir.TypeRestfulInteractionSearchSystem,
-			},
-		},
-		Context: PolicyContext{
-			DataHolderOrganizationId: "00000659",
-		},
-	}
-
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.False(t, resp.Allow)
-	assert.False(t, inp.Context.FHIRCapabilityChecked)
-}
-
-func TestComponent_allow_include(t *testing.T) {
-	input := PolicyInput{
-		Subject: Subject{
-			Properties: SubjectProperties{
-				ClientQualifications:  []string{"mcsd_query"},
-				SubjectOrganizationId: "00000666",
-			},
-		},
-		Resource: PolicyResource{
-			Type: fhir.ResourceTypeLocation,
-			Properties: PolicyResourceProperties{
-				ResourceId: "88716123",
-			},
-		},
-		Action: PolicyAction{
-			Properties: PolicyActionProperties{
-				ContentType:     "application/fhir+json",
-				InteractionType: fhir.TypeRestfulInteractionRead,
-				Include:         []string{"Location:organization"},
-			},
-		},
-		Context: PolicyContext{
-			DataHolderOrganizationId: "00000659",
-		},
-	}
-
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.True(t, resp.Allow)
-	assert.True(t, inp.Context.FHIRCapabilityChecked)
-}
-
-func TestComponent_reject_include(t *testing.T) {
-	input := PolicyInput{
-		Subject: Subject{
-			Properties: SubjectProperties{
-				ClientQualifications:  []string{"mcsd_query"},
-				SubjectOrganizationId: "00000666",
-			},
-		},
-		Resource: PolicyResource{
-			Type: fhir.ResourceTypeEndpoint,
-			Properties: PolicyResourceProperties{
-				ResourceId: "88716123",
-			},
-		},
-		Action: PolicyAction{
-			Properties: PolicyActionProperties{
-				ContentType:     "application/fhir+json",
-				InteractionType: fhir.TypeRestfulInteractionRead,
-				Include:         []string{"Endpoint:organization"},
-			},
-		},
-		Context: PolicyContext{
-			DataHolderOrganizationId: "00000659",
-		},
-	}
-
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.False(t, resp.Allow)
-	assert.False(t, inp.Context.FHIRCapabilityChecked)
-}
-
-func TestComponent_reject_revinclude(t *testing.T) {
-	input := PolicyInput{
-		Subject: Subject{
-			Properties: SubjectProperties{
-				ClientQualifications:  []string{"mcsd_query"},
-				SubjectOrganizationId: "00000666",
-			},
-		},
-		Resource: PolicyResource{
-			Type: fhir.ResourceTypePractitioner,
-			Properties: PolicyResourceProperties{
-				ResourceId: "88716123",
-			},
-		},
-		Action: PolicyAction{
-			Properties: PolicyActionProperties{
-				ContentType:     "application/fhir+json",
-				InteractionType: fhir.TypeRestfulInteractionRead,
-				Include:         []string{"Location:organization"},
-			},
-		},
-		Context: PolicyContext{
-			DataHolderOrganizationId: "00000659",
-		},
-	}
-
-	inp, resp := evalCapabilityPolicy(context.Background(), input)
-	assert.False(t, resp.Allow)
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_update")
+	assert.NotEmpty(t, resp)
 	assert.False(t, inp.Context.FHIRCapabilityChecked)
 }
 
@@ -271,8 +63,9 @@ func TestComponent_allow_interaction(t *testing.T) {
 			DataHolderOrganizationId: "00000659",
 		},
 	}
-	inp, resp := evalCapabilityPolicy(context.Background(), input)
-	assert.True(t, resp.Allow)
+
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_update")
+	assert.Empty(t, resp)
 	assert.True(t, inp.Context.FHIRCapabilityChecked)
 }
 
@@ -301,8 +94,9 @@ func TestComponent_allow_search_param(t *testing.T) {
 			DataHolderOrganizationId: "00000659",
 		},
 	}
-	inp, resp := evalCapabilityPolicy(context.Background(), input)
-	assert.True(t, resp.Allow)
+
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_update")
+	assert.Empty(t, resp)
 	assert.True(t, inp.Context.FHIRCapabilityChecked)
 }
 
@@ -331,8 +125,9 @@ func TestComponent_reject_search_param(t *testing.T) {
 			DataHolderOrganizationId: "00000659",
 		},
 	}
-	inp, resp := evalCapabilityPolicy(context.Background(), input)
-	assert.False(t, resp.Allow)
+
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_update")
+	assert.NotEmpty(t, resp)
 	assert.False(t, inp.Context.FHIRCapabilityChecked)
 }
 
@@ -344,9 +139,6 @@ func TestComponent_reject_interaction_type(t *testing.T) {
 				SubjectOrganizationId: "00000666",
 			},
 		},
-		Resource: PolicyResource{
-			Type: to.Ptr(fhir.ResourceTypeOrganization),
-		},
 		Action: PolicyAction{
 			Properties: PolicyActionProperties{
 				ContentType:     "application/fhir+json",
@@ -357,8 +149,9 @@ func TestComponent_reject_interaction_type(t *testing.T) {
 			DataHolderOrganizationId: "00000659",
 		},
 	}
-	inp, resp := evalCapabilityPolicy(context.Background(), input)
-	assert.False(t, resp.Allow)
+
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_update")
+	assert.Empty(t, resp)
 	assert.False(t, inp.Context.FHIRCapabilityChecked)
 }
 
@@ -387,8 +180,9 @@ func TestComponent_allow_include(t *testing.T) {
 			DataHolderOrganizationId: "00000659",
 		},
 	}
-	inp, resp := evalCapabilityPolicy(context.Background(), input)
-	assert.True(t, resp.Allow)
+
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_query")
+	assert.Empty(t, resp)
 	assert.True(t, inp.Context.FHIRCapabilityChecked)
 }
 
@@ -417,8 +211,9 @@ func TestComponent_reject_include(t *testing.T) {
 			DataHolderOrganizationId: "00000659",
 		},
 	}
-	inp, resp := evalCapabilityPolicy(context.Background(), input)
-	assert.False(t, resp.Allow)
+
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_query")
+	assert.NotEmpty(t, resp)
 	assert.False(t, inp.Context.FHIRCapabilityChecked)
 }
 
@@ -440,15 +235,16 @@ func TestComponent_reject_revinclude(t *testing.T) {
 			Properties: PolicyActionProperties{
 				ContentType:     "application/fhir+json",
 				InteractionType: fhir.TypeRestfulInteractionRead,
-				Include:         []string{"Location:organization"},
+				Revinclude:      []string{"Location:organization"},
 			},
 		},
 		Context: PolicyContext{
 			DataHolderOrganizationId: "00000659",
 		},
 	}
-	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input)
-	assert.False(t, resp.Allow)
+
+	inp, resp := enrichPolicyInputWithCapabilityStatement(context.Background(), input, "mcsd_query")
+	assert.NotEmpty(t, resp)
 	assert.False(t, inp.Context.FHIRCapabilityChecked)
 }
 
