bf281c74ffdb72363b0b2d833a8fb183c6be2800
Rein Krul <info@reinkrul.nl>
2026-03-19 16:02:03 +0100

    demo ehr

--- mock-components/demo-ehr/src/authConfig.js
+++ mock-components/demo-ehr/src/authConfig.js
@@ -1,6 +1,4 @@
 // Auth Configuration for demo-dezi-client
-const env = window._env_ || {};
-
 export const authConfig = {
-  baseUrl: env.AUTH_BASE_URL || process.env.REACT_APP_AUTH_BASE_URL || 'http://localhost:8090',
+  baseUrl: process.env.REACT_APP_AUTH_BASE_URL || 'http://localhost:8090',
 };
