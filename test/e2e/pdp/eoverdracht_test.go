5a0b3cca0b3bc7fd5c13308531474a0493c52551
Dirk Geurs <dirk.geurs@reaktor.com>
2026-03-23 14:46:52 +0100

    update tasks tests

--- test/e2e/pdp/eoverdracht_test.go
+++ test/e2e/pdp/eoverdracht_test.go
@@ -100,6 +100,56 @@ func Test_EOverdrachtAuthorize(t *testing.T) {
 			}`,
 			allow: false,
 		},
+		{
+			name: "authorize valid update request on e-overdracht task using the PDP",
+			pdpJSON: `{
+			  "input": {
+				"subject": {
+				  "user_id": "000095254",
+				  "user_role": "01.015",
+				  "organization_ura": "00000040",
+				  "organization_facility_type": "Z3",
+				  "scope": "eoverdracht-receiver"
+				},
+				"request": {
+				  "method": "PUT",
+				  "protocol": "HTTP/1.0",
+				  "path": "/Task/B033BD18-60A0-4648-3219-AC2A7A27C049"
+				},
+				"context": {
+				  "data_holder_organization_id": "00000030",
+				  "data_holder_facility_type": "Z3",
+				  "connection_type_code": "hl7-fhir-rest"
+				}
+			  }
+			}`,
+			allow: true,
+		},
+		{
+			name: "do not authorize updates on other resources than Tasks PDP",
+			pdpJSON: `{
+			  "input": {
+				"subject": {
+				  "user_id": "000095254",
+				  "user_role": "01.015",
+				  "organization_ura": "00000040",
+				  "organization_facility_type": "Z3",
+				  "scope": "eoverdracht-receiver"
+				},
+				"request": {
+				  "method": "PUT",
+				  "protocol": "HTTP/1.0",
+				  "path": "/Observation/7DC623BA-0EF1-42AD-0AAD-F4D034F67C9F"
+				},
+				"context": {
+				  "data_holder_organization_id": "00000030",
+				  "data_holder_facility_type": "Z3",
+				  "connection_type_code": "hl7-fhir-rest"
+				}
+			  }
+			}`,
+			allow: false,
+		},
 	}
 
 	for _, testCase := range testCases {
